747c47c022a21285b98fe6c7dab22c0482904143
Wade Shuler <wadeshuler@users.noreply.github.com>
2015-02-03 03:58:44 -0500

    Update certification file

--- ipnlistener.php
+++ ipnlistener.php
@@ -65,7 +65,7 @@ class IpnListener {
      */
     public $timeout = 30;       
     
-    private $post_data = array();
+    private $post_data;
     private $post_uri = '';     
     private $response_status = '';
     private $response = '';
@@ -97,7 +97,7 @@ class IpnListener {
 
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cert/api_cert_chain.crt');
+		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cert/ca-bundle.crt');
         curl_setopt($ch, CURLOPT_URL, $uri);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded_data);
