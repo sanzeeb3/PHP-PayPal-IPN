87def363e896974029f9a6e6affe20044dd602a0
Anthony Kuske <anthonykuske@gmail.com>
2015-06-11 01:04:50 +0200

    Option to disable SSL peer validation

--- src/IpnListener.php
+++ src/IpnListener.php
@@ -57,6 +57,13 @@ class IpnListener
      */
     public $timeout = 30;
 
+    /**
+     * If true, enabled SSL certification validation when using cURL
+     *
+     * @var boolean
+     */
+    public $verify_ssl = true;
+
 	private $_errors = array();
     private $post_data;
 	private $rawPostData;				// raw data from php://input
@@ -87,9 +94,11 @@ class IpnListener
 
         $ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cert/api_cert_chain.crt');
+        if ($this->verify_ssl) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cert/api_cert_chain.crt');
+        }
         curl_setopt($ch, CURLOPT_URL, $uri);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded_data);
