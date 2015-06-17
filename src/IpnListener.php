a67924cae52912857361b41cf6129bd8832c354a
Anthony Kuske <anthonykuske@gmail.com>
2015-06-18 01:44:22 +0200

    Fix certificate path

--- src/IpnListener.php
+++ src/IpnListener.php
@@ -97,7 +97,7 @@ class IpnListener
         if ($this->verify_ssl) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cert/api_cert_chain.crt');
+    		curl_setopt($ch, CURLOPT_CAINFO, dirname(dirname(__FILE__)) . '/cert/api_cert_chain.crt');
         }
         curl_setopt($ch, CURLOPT_URL, $uri);
         curl_setopt($ch, CURLOPT_POST, true);
